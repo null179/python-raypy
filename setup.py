05c5b61028a51ac51f10f0fcccac9c9447a53fe6
Tobias Schoch <tobias.schoch@vtxmail.ch>
2019-05-06 19:07:09 +0200

    changed package name to raypy

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
 dependency_links = [x.strip() for x in all_reqs if x.startswith('git+')]
 
 setup(
-    name='python-raypy',
+    name='raypy',
     version_format='{tag}.dev{commitcount}+{gitsha}',
     setup_requires=['setuptools-git-version','pytest-runner'],
     description='A simple simulation for 2D arrangments of basic optical elements',
